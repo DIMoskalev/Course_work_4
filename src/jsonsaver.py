1f857093c4eb205c8c223b026b756d207f6b21c4
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-04-09 20:08:34 +0300

    add file jsonsaver with __init__, get_vacancy, add_vacancy and del_vacancy methods

---
+++ src/jsonsaver.py
@@ -0,0 +1,18 @@
+import json
+
+from src.saver import Saver
+
+
+class JSONSaver(Saver):
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def get_vacancy(self, vacancies):
+        pass
+
+    def add_vacancy(self, vacancies):
+        pass
+
+    def del_vacancy(self, vacancies):
+        pass

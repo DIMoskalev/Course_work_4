67304c882a4dbed7eca27f68939cfced23b341fa
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-04-09 20:04:20 +0300

    add file config with ROOT_DIR, OPERATIONS_PATH, TEST_PATH

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+ROOT_DIR = os.path.dirname(__file__)
+
+OPERATIONS_PATH = os.path.join(ROOT_DIR, 'data', 'vacancies.json')
+
+TEST_PATH = os.path.join(ROOT_DIR, 'data', 'solo_vacancy.json')

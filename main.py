618bf83e3bcad94085ec66f9c2723817d7f094ee
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-04-15 01:11:19 +0300

    ready file main

--- main.py
+++ main.py
@@ -1,33 +1,25 @@
-from src.utlis import get_vacancies, print_vacancies, filter_vacancies, get_vacancies_by_salary, sort_vacancies, \
-    get_top_vacancies
+from src.utlis import work_with_vacancies_from_api, work_with_vacancies_from_json
 
 
 def main():
-    print("Добро пожаловать в программу поиска вакансий с сайта hh.ru.")
+    print("Добро пожаловать в программу по поиску и работе с вакансиями!")
     while True:
-        start_input = input('Чтобы начать работу программы нажмите Enter и следуйте инструкциям.\n')
-        if start_input == '':
-            search_query = input('Введите ключевое слово для поиска вакансий:\n')
-            top_n = int(input('Введите количество вакансий для вывода в топ N: '))
-            filter_words = input("Введите ключевые слова для фильтрации вакансий через пробел: ").split()
-
-            while True:
-                salary_range = input("Введите диапазон зарплат в формате '50000 - 60000':\n").split()
-                if len(salary_range) == 3:
-                    break
-
-            vacancies_list = get_vacancies(search_query)
-
-            filtered_vacancies = filter_vacancies(vacancies_list, filter_words)
-
-            ranged_vacancies = get_vacancies_by_salary(filtered_vacancies, salary_range)
-
-            sorted_vacancies = sort_vacancies(ranged_vacancies)
-
-            top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-            print_vacancies(top_vacancies)
-
-            break
+        start_input = input('Чтобы начать работу программы нажмите Enter и следуйте инструкциям.\n'
+                            'В любой момент вы можете ввести "стоп" или "stop" и программа завершит работу.\n'
+                            'Для того чтоб вернутся к выбору операций введите "back" или "назад".\n')
+        while start_input not in ['stop', 'стоп']:
+            user_input = input('Выберите, в каком виде вы хотите работать с вакансиями.\n'
+                               '1 - Программа предоставит возможность работать с вакансиями с сайта hh.ru\n'
+                               '2 - Программа предоставит возможность работать с вакансиями из json-файла\n').lower()
+            if user_input == '1':
+                work_with_vacancies_from_api()
+            elif user_input == '2':
+                work_with_vacancies_from_json()
+            elif user_input in ['stop', 'стоп']:
+                quit()
+            else:
+                print('Нужно выбрать режим работы из представленных ранее (либо 1, либо 2)\n')
+        break
 
 
 if __name__ == '__main__':

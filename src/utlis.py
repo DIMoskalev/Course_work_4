2d2ec50246ea9ef4f0061a700a7d088775838aaf
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-04-15 23:24:00 +0300

    fix some bugs

--- src/utlis.py
+++ src/utlis.py
@@ -83,67 +83,70 @@ def work_with_vacancies_from_api():
             except UnboundLocalError:
                 print('Для работы с вакансиями необходимо выполнить запрос по ключевому слову!')
                 continue
-        if user_input == '2':
-            filter_words = input("Введите ключевые слова для фильтрации вакансий через пробел: ").split()
-            vacancies_list = filter_vacancies(vacancies_list, filter_words)
-            print(f'Выполнена фильтрация вакансий по ключевым словам\n'
-                  f'Вакансий в списке: {len(vacancies_list)} шт.\n')
-        if user_input == '3':
-            while True:
-                salary_range = input("Введите диапазон зарплат в формате '50000 - 60000':\n").split()
-                if len(salary_range) == 3:
+            if user_input == '2':
+                filter_words = input("Введите ключевые слова для фильтрации вакансий через пробел: ").split()
+                vacancies_list = filter_vacancies(vacancies_list, filter_words)
+                print(f'Выполнена фильтрация вакансий по ключевым словам\n'
+                      f'Вакансий в списке: {len(vacancies_list)} шт.\n')
+            if user_input == '3':
+                while True:
+                    salary_range = input("Введите диапазон зарплат в формате '50000 - 60000':\n").split()
+                    if len(salary_range) == 3:
+                        break
+                vacancies_list = get_vacancies_by_salary(vacancies_list, salary_range)
+                print(f'Зарплаты отфильтрованы по указанному диапазону зарплат\n'
+                      f'Вакансий в списке: {len(vacancies_list)} шт.\n')
+            if user_input == '4':
+                vacancies_list = sort_vacancies(vacancies_list)
+                print(f'Вакансии отсортированы в порядке убывания минимальной зарплаты\n'
+                      f'Вакансий в списке: {len(vacancies_list)} шт.\n')
+            if user_input == '5':
+                top_n = int(input('Введите количество вакансий для вывода в топ N: '))
+                vacancies_list = get_top_vacancies(vacancies_list, top_n)
+                print(f'Вакансий в списке: {len(vacancies_list)}\n')
+            if user_input == '6':
+                print_vacancies(vacancies_list)
+            if user_input == '7':
+                file_name = input('Введите имя файла: \n')
+                file_path = os.path.join(ROOT_DIR, 'data', file_name + '.json')
+                json_file_handler = JSONFileHandler(file_path)
+                if not os.path.exists(file_path):
+                    json_file_handler.write_vacancies_to_file(vacancies_list)
+                    print('Вакансии сохранены в json-файл\n')
                     break
-            vacancies_list = get_vacancies_by_salary(vacancies_list, salary_range)
-            print(f'Зарплаты отфильтрованы по указанному диапазону зарплат\n'
-                  f'Вакансий в списке: {len(vacancies_list)} шт.\n')
-        if user_input == '4':
-            vacancies_list = sort_vacancies(vacancies_list)
-            print(f'Вакансии отсортированы в порядке убывания минимальной зарплаты\n'
-                  f'Вакансий в списке: {len(vacancies_list)} шт.\n')
-        if user_input == '5':
-            top_n = int(input('Введите количество вакансий для вывода в топ N: '))
-            vacancies_list = get_top_vacancies(vacancies_list, top_n)
-            print(f'Вакансий в списке: {len(vacancies_list)}\n')
-        if user_input == '6':
-            print_vacancies(vacancies_list)
-        if user_input == '7':
-            file_name = input('Введите имя файла: \n')
-            file_path = os.path.join(ROOT_DIR, 'data', file_name + '.json')
-            json_file_handler = JSONFileHandler(file_path)
-            if not os.path.exists(file_path):
-                json_file_handler.write_vacancies_to_file(vacancies_list)
-                print('Вакансии сохранены в json-файл\n')
-                break
-            else:
-                print(f'Файл с таким именем уже существует.')
-                user_confirm = input(f'Хотите выполнить операции с этим файлом?\n'
-                                     f'Введите "Да" или "Нет"\n').lower().strip()
-                if user_confirm == 'нет':
-                    continue
-                elif user_confirm == 'да':
-                    user_confirm = input('Выберите, что вы хотите сделать с текущим файлом:\n'
-                                         '1 - Добавить вакансии в конец текущего файла\n'
-                                         '2 - Стереть прошлые данные и записать только текущие вакансии\n')
-                    if user_confirm == '1':
-                        json_file_handler.add_vacancies(vacancies_list)
-                        print(f'Текущие вакансии добавлены в конец файла {file_name}')
-                        quit()
-                    elif user_confirm == '2':
-                        json_file_handler.write_vacancies_to_file(vacancies_list)
-                        print(f'Прошлые вакансии удалены из файла, текущие вакансии сохранены в файл {file_name}')
-                        quit()
-                    else:
-                        print('Повторите попытку операции сохранения в файл')
+                else:
+                    print(f'Файл с таким именем уже существует.')
+                    user_confirm = input(f'Хотите выполнить операции с этим файлом?\n'
+                                         f'Введите "Да" или "Нет"\n').lower().strip()
+                    if user_confirm == 'нет':
+                        continue
+                    elif user_confirm == 'да':
+                        user_confirm = input('Выберите, что вы хотите сделать с текущим файлом:\n'
+                                             '1 - Добавить вакансии в конец текущего файла\n'
+                                             '2 - Стереть прошлые данные и записать только текущие вакансии\n')
+                        if user_confirm == '1':
+                            json_file_handler.add_vacancies(vacancies_list)
+                            print(f'Текущие вакансии добавлены в конец файла {file_name}')
+                            quit()
+                        elif user_confirm == '2':
+                            json_file_handler.write_vacancies_to_file(vacancies_list)
+                            print(f'Прошлые вакансии удалены из файла, текущие вакансии сохранены в файл {file_name}')
+                            quit()
+                        else:
+                            print('Повторите попытку операции сохранения в файл')
+            if user_input in ['stop', 'стоп']:
+                quit()
         if user_input in ['stop', 'стоп']:
             quit()
-    else:
-        print('Введите порядковый номер операции, которую хотите выполнить')
+        else:
+            print('Введите порядковый номер операции, которую хотите выполнить')
 
 
 def work_with_vacancies_from_json():
     """Функция позволяет пользователю работать с вакансиями
     из json-файла"""
-    print("Добро пожаловать в программу работы с вакансиями из json-файла!\n")
+    print("Добро пожаловать в программу работы с вакансиями из "
+          "созданного в программе по работе с api hh.ru json-файла!\n")
     user_input = 0
     while user_input not in ['back', 'назад']:
         user_input = input('Выберите, какую команду хотите выполнить:\n'
@@ -164,7 +167,7 @@ def work_with_vacancies_from_json():
                 else:
                     json_file_handler = JSONFileHandler(file_path)
                     vacancies_from_file = json_file_handler.get_vacancies()
-                    vacancies_list = Vacancy.data_conversion(vacancies_from_file)
+                    vacancies_list = Vacancy.get_objects_for_data_conversion(vacancies_from_file)
                     print(f'Вакансии получены из json-файла в виде списка.\n'
                           f'Вакансий в списке: {len(vacancies_list)} шт.\n')
                     continue
@@ -238,5 +241,7 @@ def work_with_vacancies_from_json():
                     print('Совпадений вакансий в текущем списке и в json-файле не найдено\n')
             if user_input in ['stop', 'стоп']:
                 quit()
+        if user_input in ['stop', 'стоп']:
+            quit()
         else:
             print('Введите порядковый номер операции, которую хотите выполнить')

c74d5225861d7d4c5a5c056ffef187682b4c3d9b
Dmitry Moskalev <moskalev199618120@gmail.com>
2024-04-15 23:20:25 +0300

    del OPERATIONS_PATH

--- config.py
+++ config.py
@@ -2,6 +2,4 @@ import os
 
 ROOT_DIR = os.path.dirname(__file__)
 
-OPERATIONS_PATH = os.path.join(ROOT_DIR, 'data', 'vacancies.json')
-
 TEST_PATH = os.path.join(ROOT_DIR, 'data', 'solo_vacancy.json')
